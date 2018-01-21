30665db4def3cb4fb14990c83a741d52e74e470d
Robert W <robwash@gmail.com>
2018-01-21 22:20:39 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,13 @@
 def turn_count(board)
 
   counter = 0
+  turn = 0
   board.each do |character|
-    if character == "X" || "O"
-
+    if turn < board.length && (character == "X" || character "O")
+      turn += 1
       counter += 1
     end
-
-
   end
+  return turn
 
 end
